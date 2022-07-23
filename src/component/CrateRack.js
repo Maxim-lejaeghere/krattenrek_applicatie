212741c7a82953f20f1544f10d7a48f1f968f90c
Maxim Lejaeghere <maxim.lejaeghere@contribute.be>
2022-07-23 15:46:53 +0200

    Update CrateRack.js

--- src/component/CrateRack.js
+++ src/component/CrateRack.js
@@ -2,21 +2,7 @@ import React from "react";
 import Shelve from "./Shelve";
 import '../App.css';
 
-
 function CrateRack(){
-    const [board, setBoard] = useState([]);
-  
-    const [{ isOver }, drop] = useDrop(() => ({
-      accept: "crate",
-      drop: (item) => addCrateToBoard(item.id),
-      collect: (monitor) => ({
-        isOver: !!monitor.isOver(),
-      }),
-    }));
-  
-    const addCrateToBoard = (id) => {
-     const fullCrateList = FullCrateList.filter((crate) => id === crate.id);
-     setBoard(() => []);
     return(
         <div className="rack" >
             <Shelve/>
